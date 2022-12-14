283ae39931abb8786032f7f10a1a5a201ad783e0
8763625 <ksingh3625@conestogac.on.ca>
2022-12-13 23:20:06 -0500

    Initial commit

---
+++ StartPage.aspx
@@ -0,0 +1,56 @@
+﻿<!--FILE :          StarPage.aspx
+    PROJECT :       PROG2001 - A-07 : JQUERY AND JSON BASED TEXT EDITOR
+    PROGRAMMER :    Kushaldeep Singh, Yuvraj Singh
+    FIRST VERSION : 12/08/2022
+    DESCRIPTION :   the aspx file is to load the file contents that are sent from the server. and pass it to textbox area for editing
+                    if the contents are changed prompt to save or neglect changes -->
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StartPage.aspx.cs" Inherits="StartPage.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <!-- title for the page --> 
+    <title style="font-size: 40px;">Text Editor</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type = "text/javascript" src = "jQuery.js"></script>
+    <style>
+        body {
+            background-attachment:fixed;
+            background-image:url(Image.jpg);
+            background-size:auto;
+        }
+    </style>
+</head>
+<body style>
+    
+    <h3 style="text-align:center">
+        <!-- header name for the game-->
+        <strong style="font-size: 40px;">Text Editor</strong>
+    </h3>
+    <form  runat="server">
+        <div  style="text-align:center">
+             <!-- div container to ask user for their name and to process input-->
+            <span id="Message" name="Message" style="text-align:center;">Please Select a file from the drop-down list for editing -:      </span>
+           <select id="dropdown" name="dropdown">
+               
+           </select>
+            <br /><br />
+            <div>
+                <div>
+                    <textarea id="txtBox" class="textbox" name="txtBox" style="height: 250px; width: 600px;"></textarea>
+                </div>
+                <div class="statusBar">
+                    <p id="statusMessage" class="statusBarText"></p>
+                </div>
+            </div>
+            <input type="text" id="saveFileTextbox" name="saveFileTextbox" style="height: 15px; width: 120px;" />
+            <button id="save" runat="server" type="button" onclick="saveAs()">SaveAs</button>
+            <br /><br />
+            <button id="Clear" runat ="server" type="reset">Clear</button>
+            <button id="Update" runat="server" type="button" onclick="saveFile()">Update</button>
+            <br /><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ StartPage.aspx.cs
@@ -0,0 +1,168 @@
+﻿/*  File            :   StarPage.aspx
+    PROJECT :       PROG2001 - A-07 : JQUERY AND JSON BASED TEXT EDITOR
+    PROGRAMMER :    Kushaldeep Singh, Yuvraj Singh
+    FIRST VERSION : 12/08/2022
+    DESCRIPTION :   the aspx file is to load the file contents that are sent from the server. and pass it to textbox area for editing
+                    if the contents are changed prompt to save or neglect changes*/
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StartPage
+{
+   
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        //	Name	:	Page_Load()
+        //	Purpose :	Loads the page after reading code from the function
+        //	Inputs	:	object sender, EventArgs e
+        //	Outputs	:	NONE
+        //	Returns	:   void
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //page load code 
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            
+        }
+
+        //	Name	:	Submit_Click()
+        //	Purpose :	validates the input provided by the user before submitting and creating
+        //              pertinent session variables
+        //	Inputs	:	object sender, EventArgs e
+        //	Outputs	:	NONE
+        //	Returns	:   void
+        protected void Submit_Click(object sender, EventArgs e)
+        {   
+           
+        }
+
+        [WebMethod]
+        //#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        public static new string OpenFile(string fileToLoad)
+        //#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+        {
+            string returnData;  // final JSON return value
+            string fileStatus;
+            string fileContents;
+            string filepath;
+
+            try
+            {
+                filepath = HttpContext.Current.Server.MapPath("MyFiles");
+                filepath = filepath + @"\" + fileToLoad;
+
+
+                if (File.Exists(filepath))
+                {
+                    fileStatus = "Success";
+                    fileContents = File.ReadAllText(filepath);
+                }
+                else
+                {
+                    fileStatus = "Failure";
+                    fileContents = "File doesn't exist";
+                }
+            }
+            catch (Exception e)
+            {
+                // I need to return something in the JSON value to indicate the exception/hold some
+                // useful information for the user ...
+                fileStatus = "Exception";
+                fileContents = "Exception : " + e.ToString();
+            }
+
+            returnData = JsonConvert.SerializeObject(new { status = fileStatus, description = fileContents });
+            return returnData;
+        }
+
+
+        [WebMethod]
+        //#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        public static string GetFiles(string fileToLoad)
+        //#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+        {
+            string returnData = "";  // final JSON return value
+            string fileStatus = "";
+            string fileContents = "";
+            string filepath = "";
+            string[] fileEntries = {""};
+            try
+            {
+                filepath = HttpContext.Current.Server.MapPath(fileToLoad);
+                
+
+                if (filepath != null)
+                {
+                    fileEntries = Directory.GetFiles(filepath);
+                    if (fileEntries != null)
+                    {
+                        for (int i = 0; i < fileEntries.Length; i++)
+                        {
+                            fileEntries[i] = Path.GetFileName(fileEntries[i]);
+                        }
+                        fileStatus = "Success";
+                        
+                    }
+                    else
+                    {
+                        fileContents = "File error";
+                    }
+                }
+                else
+                {
+                    fileStatus = "Failure";
+                    fileContents = "File doesn't exist";
+                }
+            }
+            catch (Exception e)
+            {
+                // I need to return something in the JSON value to indicate the exception/hold some
+                // useful information for the user ...
+                fileStatus = "Exception";
+                fileContents = "Something bad happened : " + e.ToString();
+            }
+
+            returnData = JsonConvert.SerializeObject(new { status = fileStatus, fileNames = fileEntries });
+            return returnData;
+        }
+
+
+        [WebMethod]
+        //#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        public static string SaveFile(string filename, string content)
+        //#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+        {
+            string returnData = "";  // final JSON return value
+            string fileStatus = "";
+            string filepath = "";
+
+            try
+            {
+                filepath = HttpContext.Current.Server.MapPath("MyFiles");
+                filepath = filepath + @"\" + filename;
+                File.WriteAllText(filepath, content);
+                fileStatus = "File Successfully Saved";
+                
+            }
+            catch (Exception e)
+            {
+                // I need to return something in the JSON value to indicate the exception/hold some
+                // useful information for the user ...
+                fileStatus = "Exception";
+                content = "Exception : " + e.ToString();
+            }
+
+            returnData = JsonConvert.SerializeObject(new { status = fileStatus, description = content });
+            
+            return returnData;
+        }
+    }
+}
